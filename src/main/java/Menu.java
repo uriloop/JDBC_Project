5506d0247485b72b2acc33da4bc3cac0220fb5b9
uriloop <orlopez@elpuig.xeill.net>
2022-02-04 14:45:08 +0100

    menu ..

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -26,17 +26,22 @@ public class Menu {
 
         do {
             System.out.println("Choose an action:\n");
-            System.out.println("(1)- Search by name\n" +
-                    "(2)- Search by scoville\n" +
-                    "(3)- Show all pepper info\n" +
-                    "(4)- back");
+            System.out.println("(1)- Ask to database\n" +
+                    "(2)- Edit database\n" +
+                    "(3)- close app\n");
             resposta=readInt();
 
         } while (resposta!=1&&resposta!=2&&resposta!=3&&resposta!=4);
 
         switch (resposta){
-            case 1 -> searchByName();
+            case 1 -> askDatabase();
+            case 2 -> editDatabase();
+            case 3 -> closeApp();
         }
+    }
+
+    public void closeApp(){
+        /*No  fa res pero així queda més maco el */
     }
 
     /**
@@ -69,7 +74,7 @@ public class Menu {
             System.out.println("Choose an action:\n");
             System.out.println("(1)- Search by name\n" +
                     "(2)- Search by scoville\n" +
-                    "(3)- Show all pepper info\n" +
+                    "(3)- show a pepper info\n" +
                     "(4)- back");
                     resposta=readInt();
 
@@ -77,9 +82,28 @@ public class Menu {
 
         switch (resposta){
             case 1 -> searchByName();
+            case 2 -> searchByScoville();
+            case 3 -> showOnePepperInfo();
+            case 4 -> inici();
         }
 
 
+    }
+
+    /**
+     * Menu per a mostrar la informacio d'un pebrot. Imprimeix una llista de noms de pebrots i ids i demana escollir una id.
+     * opcions:
+     * 1- introduir una id
+     * 2- llistar
+     * 3- tornar enrere.
+     *
+     */
+    private void showOnePepperInfo() {
+
+
+    }
+
+    private void searchByScoville() {
     }
 
     /**
